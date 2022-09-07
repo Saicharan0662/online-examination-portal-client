a180016d415d9d0077a22ed72f183086dd54e4e5
sai charan <saicharan0662@gmail.com>
2022-09-07 19:00:03 +0530

    basic page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
