e0addac956a0fc06ccc81b8c2284b205d0753933
sai charan <saicharan0662@gmail.com>
2022-09-09 14:31:22 +0530

    separated dashboard on usertype

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Navigate } from 'react-router';
 import Navbar from '../components/Navbar';
+import ExaminerDashBoard from './ExaminerDashBoard';
+import StudentDashBoard from './StudentDashBoard';
 
 const Dashboard = () => {
 
@@ -10,13 +12,14 @@ const Dashboard = () => {
     return (
         <>
             {!user && <Navigate to='/' />}
-            <div>
-                <Navbar
-                    btnText='Logout'
-                    path='/'
-                />
-                <div>Welcome {user?.name}</div>
-            </div>
+            {/* <div> */}
+            <Navbar
+                btnText='Logout'
+                path='/'
+            />
+            {user?.userType === 'examiner' ? <ExaminerDashBoard />
+                : <StudentDashBoard />}
+            {/* </div> */}
         </>
     )
 }
---
+++ src/pages/ExaminerDashBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExaminerDashBoard = () => {
+    return (
+        <div>ExaminerDashBoard</div>
+    )
+}
+
+export default ExaminerDashBoard
